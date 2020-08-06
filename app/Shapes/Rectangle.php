ae7e304e042023d2db188296e862dcd17df5f023
deanssmart <deanssmart@gmail.com>
2020-08-06 16:52:18 +0100

    1st interface challenge completed

---
+++ app/Shapes/Rectangle.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Shapes;
+
+class Rectangle implements ShapeInterface
+{
+    private $length;
+    private $width;
+
+    public function __construct(float $length, float $width)
+    {
+        $this->length = $length;
+        $this->width = $width;
+    }
+
+    public function area() : float
+    {
+        return $this->length * $this->width;
+    }
+}
